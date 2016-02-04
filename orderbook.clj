82a84bb5e330dc9a030918251a0ce07c379ae7eb
Ben Coburn <bcoburn3@gmail.com>
2016-02-03 21:23:47 -0500

    keywords, exec ws, limit orders
    Switched to keywords instead of string keys, added separate exec websocket channel, fixed limit orders to persist in correct order book

--- orderbook.clj
+++ orderbook.clj
@@ -1,9 +1,5 @@
-(ns stockfighter-server.ob
-  (require [clj-http.client :as client]
-           [gniazdo.core :as ws]
-           [clojure.data.json :as json]
-           [clojure.math.combinatorics :as combo]
-           [clojure.core.async
+(ns forex.ob
+  (require [clojure.core.async
              :as a
              :refer [>! <! >!! <!! go go-loop chan buffer close! thread
                      alts! alts!! timeout]]))
@@ -35,76 +31,76 @@
 (defn add-order [order book]
   (assoc book [(order "price") (order "id")] order))
 
+(defn update-match-order [order fill fill-qty open?]
+  (assoc order
+         :open open?
+         :fills (conj (order :fills) fill)
+         :totalFilled (+ (order :totalFilled) fill-qty)
+         :qty (- (order :qty) fill-qty)))
+
 (defn match-order [book qty max-price dir-comp]
   ;returns a vector [remaining-qty fills filled-orders order-book]"
   ;returned order book doesn't have order to be matched, has all other
   ;effects of that order
-  (loop [fills [] filled []
+  (loop [fills [] changed-orders []
          cur-order (first book) cur-book (next book) rem-qty qty]
     (let [cur-qty (cur-order "qty")
           cur-price (cur-order "price")
           ts (System/currentTimeMillis)] ;TODO:  make this a RFC 3339 timestamp
       (if (dir-comp max-price cur-price)
         (let [match-qty (min rem-qty cur-qty)
-              match-order (assoc cur-order
-                                 "qty" (- cur-qty match-qty)
-                                 "totalFilled" (+ (cur-order "totalFilled") match-qty)
-                                 "fills" (conj (cur-order "fills")
-                                               {"qty" match-qty "price" cur-price "ts" ts}))]
+              fill {:qty match-qty :price cur-price :ts ts}
+              new-fills (conj fills fill)
+              match-order (update-match-order cur-order fill match-qty (< rem-qty cur-qty))
+              new-changed-orders (conj changed-orders match-order)]
           (cond (> rem-qty cur-qty)
-                (recur (conj fills {"qty" cur-qty "price" cur-price "ts" ts})
-                       (conj filled (assoc match-order "open" false))
-                       (first cur-book) (next cur-book) (- rem-qty cur-qty))
+                (recur new-fills new-changed-orders (first cur-book) 
+                       (next cur-book) (- rem-qty cur-qty))
                 (= rem-qty cur-qty)
-                [0
-                 (conj fills {"qty" cur-qty "price" cur-price "ts" ts})
-                 (conj filled (assoc match-order "open" false))
-                 cur-book]
+                [0 fills new-changed-orders cur-book]
                 (< rem-qty cur-qty)
-                [0
-                 (conj fills {"qty" rem-qty "price" cur-price "ts" ts})
-                 filled
-                 (assoc cur-book [cur-price (cur-order "id")] match-order)]))
-        [rem-qty fills filled (add-order cur-order cur-book) cur-order]))))
+                [0 fills new-changed-orders (add-order match-order cur-book)]))
+        [rem-qty fills filled cur-book cur-order]))))
 
-(defn make-orderbook [in-ch]
-  ;returns a channel on which order results will appear
-  (let [out-ch (chan 100)]
+(defn make-orderbook [from-frontend-ch to-mg-ch to-exec-ch]
     (go-loop [bids (sorted-map-by (comp-order >))
               asks (sorted-map-by (comp-order <))
-              msg (<! in-ch)]
-      (if (msg "cancel")
-        (let [{buy? "buy?" key "key"} msg
+              [type req-id msg] (<! from-frontend-ch)]
+      (case type
+        :cancel-req
+        (let [{buy? :buy? key :key} msg
               book (if buy? bids asks)
               order (book key)]
-          (do (>! out-ch (assoc order "open" false "qty" 0))
+          (do (>! to-mg-ch [type req-id (assoc order :qty 0 :open false)])
               (recur (if buy? (dissoc book key) bids) (if buy? asks (dissoc book key))
-                     (<! in-ch))))
-        (let [{dir "direction" order-type "orderType" price "price" qty "qty"} msg
-              buy? (= dir "buy")
+                     (<! from-frontend-ch))))
+        :place-req
+        (let [{dir :direction order-type :orderType price :price qty :qty} msg
+              buy? (= dir :buy)
               dir-comp (if buy? buy-comp sell-comp) ;function to use to see if orders match
               book (if buy? asks bids)]
-          (let [[rem-qty fills filled-orders new-book] (match-order book msg qty price dir-comp)
+          (let [[rem-qty fills changed-orders new-book] (match-order book msg qty price dir-comp)
                 new-order (assoc msg
-                                 "qty" rem-qty
-                                 "totalFilled" (- (msg "originalQty") rem-qty)
-                                 "fills" fills)]
+                                 :qty rem-qty
+                                 :totalFilled (- (msg :originalQty) rem-qty)
+                                 :fills fills)]
             (if (> rem-qty 0)
               (case order-type
-                "limit" (do (go (>! out-ch new-order))
-                            (go (>! out-ch {"ws-fills" filled-orders}))
-                            (recur (if buy? bids new-book) (if buy? new-book asks)
-                                   (<! in-ch)))
-                "fok" (do (go (>! out-ch (assoc new-order "qty" 0 "open" false)))
-                          (recur bids asks (<! in-ch))) ;order killed, recur with original values
-                "ioc" (do (go (>! out-ch (assoc new-order "qty" 0 "open" false)))
-                          (go (>! out-ch {"ws-fills" filled-orders}))
-                          (recur (if buy? bids new-book) (if buy? new-book asks)
-                                   (<! in-ch)))) 
-                                        ;market orders will be treated as IOC orders with 0 or
-                                        ;max int prices
-              (do (go (>! out-ch (assoc new-order "open" false)))
-                  (go (>! out-ch {"ws-fills" filled-orders}))
+                "limit" 
+                (do (go (>! to-mg-ch [type req-id new-order]))
+                    (go (>! to-exec-ch changed-orders))
+                    (recur (if buy? (add-order new-order bids) new-book) 
+                           (if buy? new-book (add-order new-order asks))
+                           (<! from-frontend-ch)))
+                ("fok" "fill-or-kill") 
+                (do (go (>! to-mg-ch [type req-id (assoc new-order "qty" 0)]))
+                    (recur bids asks (<! from-frontend-ch))) ;order killed, recur with original values
+                ("ioc" "immediate-or-cancel" "market")
+                (do (go (>! to-mg-ch [type req-id (assoc new-order "qty" 0)]))
+                    (go (>! to-exec-ch changed-orders))
+                    (recur (if buy? bids new-book) (if buy? new-book asks)
+                           (<! from-frontend-ch)))) 
+              (do (go (>! to-mg-ch [type req-id (assoc new-order "open" false)]))
+                  (go (>! to-exec-ch changed-orders))
                   (recur (if buy? bids new-book) (if buy? new-book asks)
-                         (<! in-ch))))))))
-    out-ch))
+                         (<! from-frontend-ch)))))))))
