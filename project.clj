9b191803e0989402ce191330198a7de037e799ec
Ben Coburn <bcoburn3@gmail.com>
2016-02-07 16:40:24 -0500

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject forex "0.2.0"
+  :description "clone of www.stockfighter.io server"
+  :url "https://github.com/bcoburn3/forex"
+  :license {:name "MIT License"
+            :url "https://github.com/bcoburn3/forex/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.async "0.2.374"]
+                 [clj-time "0.11.0"]
+                 [http-kit "2.1.18"]
+                 [compojure "1.4.0"]
+                 [ring "1.4.0"]]
+  :aot :all
+  :main forex.handler
+  :uberjar {:aot :all})
